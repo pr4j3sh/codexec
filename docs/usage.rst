543eaaa806844753f52e6a60435bb878bfe9d405
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-10-14 16:43:41 +0530

    rm -rf ./*

--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use codexec in a project::
-
-    import codexec
--- src/codexec/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for codexec."""
-
-__author__ = """prajesh"""
-__email__ = 'prajesh.eleven118@gmail.com'
-__version__ = '0.1.0'
